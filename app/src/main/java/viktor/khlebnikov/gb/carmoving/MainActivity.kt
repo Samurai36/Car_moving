e189f58b9a3c401ee605c9ef030618117696961e
Samurai36 <svry88@gmail.com>
2022-04-01 20:24:18 +0300

    добавил метод для передвижения view

--- app/src/main/java/viktor/khlebnikov/gb/carmoving/MainActivity.kt
+++ app/src/main/java/viktor/khlebnikov/gb/carmoving/MainActivity.kt
@@ -1,12 +1,34 @@
 package viktor.khlebnikov.gb.carmoving
 
-import androidx.appcompat.app.AppCompatActivity
+import android.animation.Animator
 import android.os.Bundle
+import android.view.View
+import android.view.animation.AccelerateDecelerateInterpolator
+import android.view.animation.TranslateAnimation
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.ViewPropertyAnimatorListener
+import viktor.khlebnikov.gb.carmoving.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.car.setOnClickListener {
+            startMoving(binding.car)
+        }
     }
-}
 
+    private fun startMoving(view: View) {
+        val animation = TranslateAnimation(0f, 500f, 0f, 0f)
+        animation.duration = 1000
+        animation.fillAfter = true
+        view.startAnimation(animation)
+    }
+
+}
